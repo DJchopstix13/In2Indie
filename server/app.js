80a04fc2b2d14e08e566cb6898d61d1e23da384c
DJChopstix13 <jpiatos13@gmail.com>
2014-03-18 15:14:26 -0700

    Attempting to utilize joomla extension for work

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ var everyauth = require('everyauth');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -28,20 +28,20 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-console.log('Loading models')
+/*console.log('Loading models')
 fs.readdirSync(__dirname + '/models').forEach(function (file) {
   if (file.match('.js$')) {
     require(__dirname + '/models/' + file);
   }
-});
-
+});*/
+/*
 console.log('Loading controllers')
 fs.readdirSync(__dirname + '/controllers').forEach(function (file) {
   if (file.match('.js$')) {
     require(__dirname + '/controllers/' + file)(app);
   }
 });
-
+*/
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });

a7ba72c41816d423e1b2c24a4e0ff30c2e7908cf
Jonathan Piatos <jpiatos13@gmail.com>
2014-04-21 22:48:07 -0700

    formatting code

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
-var express = require('express')
-  , routes  = require('./routes')
-  , user    = require('./routes/user')
-  , http    = require('http')
-  , path    = require('path')
-  , db      = require('./models')
- 
-var app = express()
- 
+var express = require('express'),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    http = require('http'),
+    path = require('path'),
+    db = require('./models')
+
+    var app = express()
+
 // all environments
 app.set('port', process.env.PORT || 80)
 app.set('views', __dirname + '/views')
@@ -17,21 +17,21 @@ app.use(express.bodyParser())
 app.use(express.methodOverride())
 app.use(app.router)
 app.use(express.static(path.join(__dirname, 'public')))
- 
+
 // development only
 if ('development' === app.get('env')) {
-  app.use(express.errorHandler())
+    app.use(express.errorHandler())
 }
- 
+
 app.get('/', routes.index)
 //app.get('/users', user.list)
- 
-db.sequelize.sync().complete(function(err) {
-  if (err) {
-    throw err
-  } else {
-    http.createServer(app).listen(app.get('port'), function(){
-      console.log('Express server listening on port ' + app.get('port'))
-    })
-  }
+
+db.sequelize.sync().complete(function (err) {
+    if (err) {
+        throw err
+    } else {
+        http.createServer(app).listen(app.get('port'), function () {
+            console.log('Express server listening on port ' + app.get('port'))
+        })
+    }
 })

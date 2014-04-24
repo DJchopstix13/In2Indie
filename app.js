1edfd991e442572e4ce24c49826094e8839d150e
Jonathan Piatos <jpiatos13@gmail.com>
2014-04-24 14:56:38 -0700

    attempting to change directory setup to ignore multiple sub-app

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 /*
- * In2Indie App
- * 
+ * In2Indie Web Application
+ *
  */
 
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var stylus = require('stylus');
+var express = require('express'),
+    routes = require('./server/routes'),
+    user = require('./server/routes/user'),
+    http = require('http'),
+    path = require('path'),
+    db = require('./server/models'),
+    stylus = require('stylus');
+
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(express.favicon(path.join(__dirname, 'public/icons/in2indie.png')));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
@@ -21,32 +25,23 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Server and Web-Client sub- application here
-//Option #1:
-//app.use('./server/', require('./server/app.js').app)
-  //  use('./web-client/', require('./web-client/app.js').app)
-    //listen(3000);
+// development only
+if ('development' === app.get('env')) {
+    app.use(express.errorHandler());
+}
 
-//Server and Web-Client sub- application here
-//Option #2:
-app.configure(function () {
-	app.use(express.compress());
-    app.use(express.logger('dev'));
-    app.set('json spaces',0);
-    app.use(express.limit('2mb'));
-    app.use(express.bodyParser());
-    app.use('./server/app.js', app.router);
-    app.use('./web-client/app.js', app.router);
-    app.use(function(err, req, res, callback){
-        res.json(err.code, {});
-    });
+app.get('/', routes.index);
+//app.get('/users', user.list)
+
+db.sequelize.sync().complete(function (err) {
+    if (err) {
+        throw err;
+    } else {
+        http.createServer(app).listen(app.get('port'), 
+        	function () {
+                console.log('Express server listening on port ' + app.get('port'));
+            }
+        )
+    }
 })
 
-// development only
-/*if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
-};*/
-
-http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
-});

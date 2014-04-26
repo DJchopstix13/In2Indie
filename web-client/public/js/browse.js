7420621dc5f909151968186c65dfe4e63b4fec30
Jonathan <jpiatos13@gmail.com>
2014-04-26 02:30:52 -0700

    added some js files for web-app functionality

---
+++ web-client/public/js/browse.js
@@ -0,0 +1,58 @@
+//browse.js
+$(function () {
+
+    $("#find-products").click(function () {
+
+        $(".product-capsule").remove();
+        var newContent = '';
+        var searchTerm = document.getElementById('prependDropdownButton').value;
+        var searchUrl = "";
+        var url = "http:98.158.149.241";
+        
+
+        if (document.getElementById('search-menu').value == 'name') {
+            searchUrl = url + "products?name=";
+        }
+        if (document.getElementById('search-menu').value == 'type') {
+            searchUrl = url + "products?type=";
+        }
+
+        $.ajax({
+            url: searchUrl + searchTerm,
+            cache: false
+        }).done(function (products) {
+            products.forEach(function (product) {
+                newContent += '<div class="product-capsule">';
+                newContent += '<div class="capsule-top">';
+                newContent += '<a href="' + url + 'product/' + product._id + '">' + product.name
+                newContent += '</a>';
+                newContent += '</div>';
+                newContent += '<div class="capsule-image">';
+                newContent += '<img src="' + product.images[160] + '">';
+                newContent += '</img>';
+                newContent += '</div>';
+                newContent += '<div class="capsule-hover">';
+                newContent += '<p>' + (product.description || "No description available");
+                newContent += '</p>';
+                newContent += '</div>';
+                newContent += '</div>';
+            })
+            $("#main-content").append(newContent);
+            $(".product-capsule").hover(
+                function () {
+                    $(this).find(".capsule-hover").css("display", "inline");
+                },
+                function () {
+                    $(this).find(".capsule-hover").css("display", "none");
+                }
+            );
+        })
+    })
+
+    $("#prependDropdownButton").keypress(function (e) {
+        if (e.which == 13) {
+            $("#find-products").click();
+        }
+    });
+
+});

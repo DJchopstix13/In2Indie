7cdd1026986cb6bdbddb2cd0014f5ddc2ac6d8fd
Jonathan <jpiatos13@gmail.com>
2014-05-04 15:02:36 -0700

    fixing sessions to work with heroku domain

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ passport.use(new FacebookStrategy({
 passport.use(new TwitterStrategy({
   consumerKey: TWITTER_APP_ID,
   consumerSecret: TWITTER_APP_SECRET,
-  callbackURL: 'http://in2indie.herokuapp.com/auth/twitter'
+  callbackURL: 'http://in2indie.herokuapp.com/auth/twitter/callback'
 }, function(accessToken, refreshToken, profile, done) {
   process.nextTick(function() {
     done(null, profile);

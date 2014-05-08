a6e3ef4af01d2561c6312911420fb53dbef64419
Jonathan <jpiatos13@gmail.com>
2014-05-08 04:40:49 -0700

    made more product sample data

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 /*
  * In2Indie App
- * 
+ *
  */
-
 /*Define environment variables*/
 var express = require('express'),
     routes = require('./server/routes'),
@@ -11,12 +10,12 @@ var express = require('express'),
     path = require('path'),
     db = require('./server/models'),
     controllers = require('./web-client/controllers/main'),
-    stylus = require('stylus'), 
-    passport = require('passport'), 
-    flash = require('connect-flash'), 
+    stylus = require('stylus'),
+    passport = require('passport'),
+    flash = require('connect-flash'),
     pg = require('pg'),
     FacebookStrategy = require('passport-facebook').Strategy;
-    TwitterStrategy = require('passport-twitter').Strategy;
+TwitterStrategy = require('passport-twitter').Strategy;
 
 //facebook id info
 var FACEBOOK_APP_ID = '673874075999542';
@@ -34,7 +33,7 @@ app.set('views', path.join(__dirname, 'web-client', 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon(path.join(__dirname, 'public/img/favicon.png')));
 app.use(express.logger('dev'));
-app.use(express.cookieParser());  
+app.use(express.cookieParser());
 app.use(express.bodyParser());
 app.use(express.json());
 app.use(express.urlencoded());
@@ -43,40 +42,44 @@ app.use(express.methodOverride());
 //initiliaize passport
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.session({ secret: 'secret' }));
+app.use(express.session({
+    secret: 'secret'
+}));
 
 //using facebook auth
 passport.use(new FacebookStrategy({
-  clientID: FACEBOOK_APP_ID,
-  clientSecret: FACEBOOK_APP_SECRET,
-  callbackURL: 'http://54.187.138.161/auth/facebook/callback'
-}, function(accessToken, refreshToken, profile, done) {
-  process.nextTick(function() {
-    done(null, profile);
-  });
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: 'http://54.187.138.161/auth/facebook/callback'
+}, function (accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+        done(null, profile);
+    });
 }));
 
 passport.use(new TwitterStrategy({
-  consumerKey: TWITTER_APP_ID,
-  consumerSecret: TWITTER_APP_SECRET,
-  callbackURL: 'http://54.187.138.161/auth/twitter/callback'
-}, function(accessToken, refreshToken, profile, done) {
-  process.nextTick(function() {
-    done(null, profile);
-  });
+    consumerKey: TWITTER_APP_ID,
+    consumerSecret: TWITTER_APP_SECRET,
+    callbackURL: 'http://54.187.138.161/auth/twitter/callback'
+}, function (accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+        done(null, profile);
+    });
 }));
- 
-passport.serializeUser(function(user, done) {
-  done(null, user);
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
 });
- 
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
+
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
 });
 
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'web-client', 'public')));
-app.use(express.session({ secret: 'secret' }));
+app.use(express.session({
+    secret: 'secret'
+}));
 app.use(flash());
 
 require('./web-client/controllers/main')(app);
@@ -91,8 +94,8 @@ app.get('/auth/facebook/callback', passport.authenticate('facebook', {
 //twitter authentication
 app.get('/auth/twitter', passport.authenticate('twitter'));
 app.get('/auth/twitter/callback', passport.authenticate('twitter', {
-  successRedirect: '/success',
-  failureRedirect: '/error'
+    successRedirect: '/success',
+    failureRedirect: '/error'
 }));
 
 //development only
@@ -100,24 +103,13 @@ if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 };
 
-//using heroku sample code for postgresql connection
-/*pg.connect(process.env.DATABASE_URL, function (err, client, done) {
-    client.query('SELECT * FROM your_table', function (err, result) {
-        done();
-        if (err) return console.error(err);
-        console.log(result.rows);
-    });
-});*/
-
-
-
 //Error handling for server side
 db.sequelize.sync().complete(function (err) {
     if (err) {
         throw err;
     } else {
         http.createServer(app).listen(app.get('port'),
-                function () {
+            function () {
                 console.log('Express server listening on port ' + app.get('port'));
             }
         )
